a8b9a266bbfeb6a7197fafe3e28272ca92a7f304
gnwncpta <gnwncpta@gmail.com>
2021-08-01 03:35:48 +0700

    add file

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ Convert .txt file to .json file
 
 ![alt text](https://i.imgur.com/nYUwci7.jpg "JavaScript") 
 ![alt text](https://i.imgur.com/AvoEJhZ.png "Node.js") 
+
+
+How to use:
+- Provide the file you want to convert named with plain.txt
+- Provide with the format like this Full Name | First Name | Last Name | Email
---
+++ index.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const chalk = require('chalk');
+let globalData = [];
+
+fs.readFile('./plain.txt', { encoding: 'utf-8' }, (err, data) => {
+    if(err) throw err;
+
+    console.log(chalk.green('✅ Success read data from plain.txt\n'));
+    let arr = data.split('\n');
+
+    arr.forEach(item => {
+        let spl = item.split(' | ');
+        const [ fullName, firstName, lastName, email ] = spl;
+
+        globalData.push(
+            {
+                "full_name": fullName,
+                "first_name": firstName,
+                "last_name": lastName,
+                "email": email
+            }
+        );
+    });
+
+    const result = {
+        data: globalData
+    }
+
+    const puft = new Uint8Array(Buffer.from(JSON.stringify(result)));
+
+    fs.writeFile('result.json', puft, (err) => {
+        if(err) throw err;
+
+        console.log(chalk.green('✅ The file has saved to result.json'));
+    });
+
+});
