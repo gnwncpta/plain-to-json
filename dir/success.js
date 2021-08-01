4670fddea23911e7bbeef7a91ec469675b028863
gnwncpta <gnwncpta@gmail.com>
2021-08-01 14:10:57 +0700

    add success console

--- dir/success.js
+++ dir/success.js
@@ -1,4 +1,5 @@
 const chalk = require('chalk');
+
 function success(){
     return console.log(`${chalk.green('[')} Successfully Created ${chalk.green(']')}\nFile name: ${chalk.bgGreen.black(' result.json ')}\n`);
 }
